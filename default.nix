72e875af96d444e21f2a9d78eb8075ed92a2d9f5
sorki <srk@48.io>
2024-06-30 20:37:58 +0200

    drop redundant stuff, modernize

--- default.nix
+++
@@ -1,2 +0,0 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc864" }:
-nixpkgs.haskell.packages.${compiler}.callPackage ./data-prometheus.nix { }
--- shell.nix
+++
@@ -1,2 +0,0 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc864" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env
