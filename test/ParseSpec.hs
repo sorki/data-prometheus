13e95b05c877ee3bb5c43c5e8658e241c42f1a03
sorki <srk@48.io>
2024-07-01 13:53:06 +0200

    add failing reference example parsing test

--- test/ParseSpec.hs
+++ test/ParseSpec.hs
@@ -6,7 +6,7 @@ module ParseSpec where
 
 import SpecHelper
 import Text.RawString.QQ
-import Data.Attoparsec.ByteString.Char8
+import Data.Either (isRight)
 import qualified Data.Map
 
 gauge = [r|# HELP go_memstats_alloc_bytes Number of bytes allocated and still in use.
@@ -45,6 +45,46 @@ node_boot_time_seconds 1.537903224e+09
 # HELP node_context_switches_total Total number of context switches.
 # TYPE node_context_switches_total counter
 node_context_switches_total 1.54296968e+08
+|]
+
+-- from https://prometheus.io/docs/instrumenting/exposition_formats/#text-format-example
+ref = [r|
+# HELP http_requests_total The total number of HTTP requests.
+# TYPE http_requests_total counter
+http_requests_total{method="post",code="200"} 1027 1395066363000
+http_requests_total{method="post",code="400"}    3 1395066363000
+
+# Escaping in label values:
+msdos_file_access_time_seconds{path="C:\\DIR\\FILE.TXT",error="Cannot find file:\n\"FILE.TXT\""} 1.458255915e9
+
+# Minimalistic line:
+metric_without_timestamp_and_labels 12.47
+
+# A weird metric from before the epoch:
+something_weird{problem="division by zero"} +Inf -3982045
+
+# A histogram, which has a pretty complex representation in the text format:
+# HELP http_request_duration_seconds A histogram of the request duration.
+# TYPE http_request_duration_seconds histogram
+http_request_duration_seconds_bucket{le="0.05"} 24054
+http_request_duration_seconds_bucket{le="0.1"} 33444
+http_request_duration_seconds_bucket{le="0.2"} 100392
+http_request_duration_seconds_bucket{le="0.5"} 129389
+http_request_duration_seconds_bucket{le="1"} 133988
+http_request_duration_seconds_bucket{le="+Inf"} 144320
+http_request_duration_seconds_sum 53423
+http_request_duration_seconds_count 144320
+
+# Finally a summary, which has a complex representation, too:
+# HELP rpc_duration_seconds A summary of the RPC duration in seconds.
+# TYPE rpc_duration_seconds summary
+rpc_duration_seconds{quantile="0.01"} 3102
+rpc_duration_seconds{quantile="0.05"} 3272
+rpc_duration_seconds{quantile="0.5"} 4773
+rpc_duration_seconds{quantile="0.9"} 9001
+rpc_duration_seconds{quantile="0.99"} 76656
+rpc_duration_seconds_sum 1.7560473e+07
+rpc_duration_seconds_count 2693
 |]
 
 testCases = [
@@ -155,7 +195,10 @@ testCases = [
 spec :: Spec
 spec = do
   it "parses samples" $ do
-    mapM_ (\(x, y) -> parseOnly parseMetrics x `shouldBe` y) testCases
+    mapM_ (\(x, y) -> parseProm x `shouldBe` y) testCases
+
+  xit "parses reference"
+    $ parseProm ref `shouldSatisfy` isRight
 
 main :: IO ()
 main = do
