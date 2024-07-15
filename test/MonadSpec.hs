149ca12b45134536dbfd0c5520b0605907967656
sorki <srk@48.io>
2024-07-15 18:56:38 +0200

    fix tests

--- test/MonadSpec.hs
+++ test/MonadSpec.hs
@@ -6,43 +6,52 @@ import SpecHelper
 import Data.Function ((&))
 import qualified Data.Map as M
 
-complex = metric "test"
-            & sub "sub1" & sub "sub2"
-            & desc "desc" & label "a" "b"
+complex :: MetricId -> MetricId
+complex =
+    sub "sub2"
+  . sub "sub1"
+  . desc "desc"
+  . label "a" "b"
+
+complexExpected :: MetricId
+complexExpected = complex $ metric "test"
 
 spec :: Spec
 spec = do
   it "generates metrics" $ do
-    runMetrics $ addMetric (metric "test") (Counter 7)
+    runMetrics (metric "test") $ addMetric' id (mkCounter 7)
     `shouldBe`
     "# HELP test \n# TYPE test counter\ntest 7.0\n"
 
   it "generates complex metrics" $ do
-    runMetrics $ addMetric complex (Counter 7)
+    runMetrics (metric "test") $ addMetric' complex (mkCounter 7)
     `shouldBe`
     "# HELP test_sub1_sub2 desc\n# TYPE test_sub1_sub2 counter\ntest_sub1_sub2{a=\"b\"} 7.0\n"
 
   it "adds errors" $ do
-    runMetrics $ addMetric (metric "test") (Counter 7) >> logError "fail"
+    runMetrics (metric "test") $ addMetric' id (Counter 7) >> logError "fail"
     `shouldBe`
     "# HELP test \n# TYPE test counter\ntest 7.0\n# ERROR fail\n"
 
   it "roundtrips complex" $ do
-    parseProm (runMetrics (addMetric complex (Counter 7)))
+    parseProm (runMetrics (metric "test") (addMetric' complex (Counter 7)))
     `shouldBe`
-    Right (M.fromList [(complex, Counter 7)])
+    Right (M.fromList [(complexExpected, Counter 7)])
 
   it "roundtrips complex handles errors" $ do
-    parseProm (runMetrics (addMetric complex (Counter 7) >> logError "nada"))
+    parseProm (runMetrics (metric "test") (addMetric' complex (Counter 7) >> logError "nada"))
     `shouldBe`
-    Right (M.fromList [(complex, Counter 7)])
+    Right (M.fromList [(complexExpected, Counter 7)])
 
   it "roundtrips multiple complex" $ do
-    parseProm (runMetrics (addMetric complex (Counter 7) >> addMetric (complex & sub "xy") (Gauge 13.37)))
+    parseProm
+      (runMetrics (metric "test") $ do
+        addMetric' complex (Counter 7)
+        addMetric' (sub "xy" . complex) (Gauge 13.37)
+      )
     `shouldBe`
-    Right (M.fromList [(complex, Counter 7), (complex & sub "xy", Gauge 13.37)])
+    Right (M.fromList [(complexExpected, Counter 7), (complexExpected & sub "xy", Gauge 13.37)])
 
 main :: IO ()
 main = do
   hspec spec
-
